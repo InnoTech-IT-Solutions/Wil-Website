b0e469df0970e405e5f3210876ac7b5300d5b64c
Tashagovender <st10210644@vcconnect.edu.za>
2024-11-16 19:23:35 +0200

    Website-Final

--- Heal-Foundation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Heal_Foundation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
